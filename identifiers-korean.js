0ef4df8f399be690a61fdd765171e99869c8e673
Fred Zenker <fzenker@hawaii.edu>
2021-06-12 01:19:56 -1000

    add files

---
+++ identifiers-korean.js
@@ -0,0 +1,64 @@
+// participant identifiers for fred's study titled x210324_korean
+
+// unassigned:
+
+    list1 = [
+        '50hu41331|jz20dgv61|y6c1a91s1|tm6jklun1|4qpw84ur1|08ljaslp1|nguhwn2k1|lt4qhrgn1|pt8z1mss1|0j82y7zj1|wal149ly1|7vb7ldek1|72fwjhn61|aauatsus1|tz9yy2sn1|ot7wd2ye1|905r90qk1|zwksazcp1|w6h9tbbm1|bc93geuy1|t86mkrzt1|gpvyjhns1|ovtoguuv1|cg9ydkyf1|2mtdba5r1|jgnfao5m1|q3vfl3be1|846k61l41|t1fb17kv1|5c2zqo691|jnu8t9ts1|yc5kz6jy1|tps0u2l61|yd67vd3w1|k7gvtkb21|7dk8712m1|pgvun1bw1|5ksdppnn1|8l25d3k81|kd6e1v711|ns2b2hgf1|3mxyqjck1|xxzwes371|sbm1ygqm1|30wfet1z1|yuptq0491|'
+    ];
+    list2 = [
+        '5vwgh2qk2|k18p9cja2|90haz13c2|9ojcp1ze2|9p7ohxou2|caybglz52|nt3hgbhx2|wy5g5lvq2|lq9nquvs2|trhhq9rt2|kmy4fabq2|yk8a98a62|5unvxtv62|rcoacpe62|aysg40ag2|j1gom6ru2|033jx6gn2|ftdhklax2|08198dqq2|jrqht6ev2|rtr07ae92|lzq4w8hg2|z8vbe6as2|d5rg8m1z2|3ohcv3er2|gpqge9ny2|r8b35mpm2|axh6nmkc2|x9f2ch4u2|7vdldwpb2|xq7a0o7o2|81keuxh42|l2dw4uda2|fcc6aza12|sx4d82f42|vs153x942|nymvtn3w2|f1v60jzf2|eyk0puvx2|zz1hm9y22|qcs35pfv2|rx2b9afg2|wjhhlaqv2|05nnm4922|cr4sccl12|4fa3ylkl2|'
+    ];
+    list3 = [
+        'o7dtghn03|aav3okdy3|0urhmf823|t4jg16l73|oop6qfd13|ze0wt2673|50n2vbpo3|zc4nbxo93|vj6aleg73|5te2jrd23|rznznv5o3|vdpfscky3|4yzsbawl3|9c7o4d9c3|e1enva893|16av2p7f3|yr3ncm5g3|y6yd38yy3|qo9r1tmh3|8b9nd01l3|5e072t0x3|r8uf0n6s3|4sppvwx63|fnpvx3yj3|1fdht9wb3|7ua9cscw3|qwuvzf593|e1gu4pqh3|ozpzsko93|953hrw0o3|z314txre3|wpbpl64b3|hf0e0hjo3|lbjad95n3|ovpuhfwx3|0j4batzr3|u58hun9d3|orkqxb4a3|kmvqmuwb3|c66d0gaf3|r4j5hcff3|a8bbbu0a3|w1ptdgl73|ugl6yw8b3|72rg7zv73|m2odhzzl3|'
+    ];
+    list4 = [
+        'r7bclt044|sg7fmuxh4|uf4m4p9b4|61ngbqlw4|6z9m4cky4|2psa1x414|y75lvubu4|65u9ugrr4|2hygmsrn4|tv6z8yel4|a3eamofm4|xq4d5l3n4|wzqsfr8c4|02euuhrs4|ut8o1or24|we8h7bew4|vam5xrmw4|1wp86n4g4|xbgohmak4|d3wbclar4|glj2j4ew4|838psb4j4|ggsbrf384|n7dkyzq64|39xsnuhv4|zzkdx5q74|yya2k8s34|b6ljdd9f4|ak8jqzkq4|rbw6qsy74|p8y4m3ah4|9skg6wtc4|sm2zwplg4|8hoekbhb4|cvljpt054|579ghlyv4|rulxh1ut4|8g4wl9ko4|1oo7nr404|e6574qyn4|67fge42o4|k9umc0jr4|b3791fxq4|qqjgusna4|4m9yporw4|1lvv6ogc4|'
+    ];
+    list5 = [
+        'vx1rkgyp5|7ub8cfxx5|xb8nn1vh5|75adkaly5|0o2pwwyq5|pdyq0f3p5|4h5vxeh25|nwu5okns5|3uwex8sl5|63k5fftw5|0lp9cw7l5|qspnvlqy5|yp71tr2u5|pvvf0ou35|l4qvqggl5|o53h434j5|kaxqjpm35|6y8k67bn5|kzckpokw5|6u7y69yo5|0p23mntd5|gnb3l73v5|e208u7j75|2npjjvmh5|g2a9grh25|zu2yavjm5|t08h424q5|l9a4tybn5|1syrgkqm5|ll34mh8m5|ae6y2akr5|vrnd2y8n5|tebwrb1h5|4a8xx9qe5|gh8pd3p85|7ou2y2w15|z2298szu5|uo3x8xuh5|scj3ycpv5|et539dn65|aljq73yc5|1bcyy3hm5|sla5726j5|yqkvmg3n5|zjx8nddl5|lxtpkj3l5|'
+    ];
+    list6 = [
+        'ut7wqvo66|nc0trftk6|txzbfxwc6|spjkw8bc6|ntybzo3p6|j66z3c456|yql0k20y6|ee96rzgu6|9szazvv16|refnfteu6|l18xzgsd6|26k0wnfr6|ca3fxaqt6|mwh477kp6|3jzkte1z6|6ckw1kov6|hxx3nfa06|8bdtlxms6|7ubnc7pq6|k335eaxd6|n6n0ksxo6|1dzfue8a6|gsve87un6|th4urgun6|2y4la7436|dw4vety26|ph14ptp16|8j240pwy6|672zu0wk6|p8bjok5u6|m0ud0ze66|autao9z56|flwqjlex6|7p6ln3o66|olhr2ma56|3n7xkgf96|rb4l84bv6|gufcly1e6|szjga5yt6|aq9eokda6|f6fg2aok6|gafpnrdf6|stdu8fuc6|1chgjjox6|t2q7n8jq6|jkw2euyz6|sksmmtpn6|'
+    ];
+
+// assigned:
+
+    list1 = [
+        '602v7c0j1|p5td3ca71|bydd1j791|pe0g7wld1|' 
+    ];
+    list2 = [
+        '60jrsq6p2|nkrev6el2|d91hfr692|bboqbfmg2|' 
+    ];
+    list3 = [
+        'm51bwc323|x261csyd3|pg8eo78s3|ugdyx0q13|' 
+    ];
+    list4 = [
+        'geyp1uwf4|820o8jew4|0ko17tly4|jr4ndfbc4|' 
+    ];
+    list5 = [
+        'osf4sw075|sznc50575|s160qtsw5|w2d24hnz5|' 
+    ];
+    list6 = [
+        '834f8tdv6|ssfzmwvk6|48wu3c1m6|' 
+    ];
+
+// completed:
+
+list1 = [
+    '602v7c0j1|' 
+];
+list2 = [
+    '60jrsq6p2|d91hfr692|' 
+];
+list3 = [
+    'm51bwc323|pg8eo78s3|' 
+];
+list4 = [
+    '820o8jew4|' 
+];
+list5 = [
+    'osf4sw075|sznc50575|' 
+];
+list6 = [
+    '' 
+];
